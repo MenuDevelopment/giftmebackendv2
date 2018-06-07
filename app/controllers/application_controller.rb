f64acb48f90eabd86394a3ad0059fe4de93161ba
Manuel Grullon <astermanuelg@gmail.com>
2018-06-07 11:46:50 -0400

    beginning to set up auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+  def jwt_password
+    ENV["JWT_PASSWORD"]
+  end
+
+
+  def generate_token(user)
+    JWT.encode({ "user_id": user.id }, jwt_password, 'HS256')
+  end
+
+  def token_json(user)
+    {
+      email: user.email,
+      user_id: user.id,
+      token: generate_token(user)
+    }
+  end
+
+  def try_decode_token
+
+
+  end
+
+  def valid_token?
+    !!try_decode_token
+  end
 end
