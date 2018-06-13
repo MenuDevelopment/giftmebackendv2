471bfef3d4c23538a1955863abc03e623565f363
Manuel Grullon <astermanuelg@gmail.com>
2018-06-13 06:41:36 -0400

    added giftcontroller#show

--- app/controllers/API/V2/gifts_controller.rb
+++ app/controllers/API/V2/gifts_controller.rb
@@ -3,4 +3,9 @@ class Api::V2::GiftsController < ApplicationController
   def index
     render json: Gift.all
   end
+
+  def show
+    @gift = Gift.find(params[:id])
+    render json: @gift
+  end
 end
