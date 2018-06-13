4735dc23f5dda39e4e15e36c76d9fc707c42bce7
Manuel Grullon <astermanuelg@gmail.com>
2018-06-13 09:41:31 -0400

    returned pledges with gifts

--- app/serializers/gift_serializer.rb
+++ app/serializers/gift_serializer.rb
@@ -1,6 +1,6 @@
 class GiftSerializer < ActiveModel::Serializer
   attributes :id, :item_name, :item_link, :item_description, :item_price, :end_date, :user_id, :pledge_amount
-
+  has_many :pledges
   def pledge_amount
     sum = 0.0
     object.pledges.each { |e| sum = sum + e.amount  }
