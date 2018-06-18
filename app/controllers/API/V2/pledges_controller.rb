385a4bdd9ed0fa46f9a73089a903e07a62f04dd4
Manuel Grullon <astermanuelg@gmail.com>
2018-06-18 17:07:16 -0400

    beginning to set up the backend to receive/update pledges

--- app/controllers/API/V2/pledges_controller.rb
+++ app/controllers/API/V2/pledges_controller.rb
@@ -1,2 +1,26 @@
 class Api::V2::PledgesController < ApplicationController
+  def create
+    @pledge = Pledge.new(pledge_params)
+    if @pledge.save
+      render json: @pledge
+    else
+      render json: {error: "Something went wrong with pledge creation"}
+    end
+  end
+
+  def update
+    @pledge = Pledge.find(params[:id])
+    @pledge.update(pledge_params)
+    if @pledge.save
+      render json: @pledge
+    else
+      render json: {error: "Something went wrong with update"}
+    end
+  end
+
+  private
+
+  def pledge_params
+    params.require(:pledge).permit(:amount, :message, :user_id, :gift_id)
+  end
 end
