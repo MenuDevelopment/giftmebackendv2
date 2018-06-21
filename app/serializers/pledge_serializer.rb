7022746c0b4042a3e953b2ddc29c106080987cb8
Manuel Grullon <astermanuelg@gmail.com>
2018-06-21 09:46:15 -0400

    change to pledge serializer to give contributors names

--- app/serializers/pledge_serializer.rb
+++ app/serializers/pledge_serializer.rb
@@ -1,5 +1,9 @@
 class PledgeSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :message
+  attributes :id, :amount, :message, :user
   has_one :user
   has_one :gift
+
+  def user
+    {id: object.user.id, first_name: object.user.first_name, last_name: object.user.last_name}
+  end
 end
